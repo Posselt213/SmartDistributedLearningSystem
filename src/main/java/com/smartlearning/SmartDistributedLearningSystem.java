4d17aac01f8d66d12c9dbdb4f191b64ddcf5137b
Emanuel Posselt <posselt213@gmail.com>
2026-03-30 12:26:58 +0100

    Setting up Project

---
+++ src/main/java/com/smartlearning/SmartDistributedLearningSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.smartlearning;
+
+/**
+ *
+ * @author comin
+ */
+public class SmartDistributedLearningSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
